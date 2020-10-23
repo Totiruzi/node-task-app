e5659ea13ead351eb257149700aabd634240ba9e
Totiruzi <totiruzi@yahoo.com>
2020-10-23 11:44:06 +0300

    Create end points to get all users and tasks and fetch individual user and task

--- src/index.js
+++ src/index.js
@@ -13,10 +13,34 @@ app.post('/users', (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((error) => {
       res.statu(400).send('Error!! ', error);
+    });
+});
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((error) => {
+      res.status(500).send('Error!!!', error);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(500).send();
     });
 });
 
@@ -25,10 +49,34 @@ app.post('/tasks', (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch((error) => {
       res.status(400).send('Error!!! ', error);
+    });
+});
+
+app.get('/tasks', (req, res) => {
+  Tasks.find({})
+    .then((task) => {
+      res.status(201).send(task);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+  Tasks.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(400).send();
+      }
+      res.send(task);
+    })
+    .catch((error) => {
+      res.status(500).send();
     });
 });
 
