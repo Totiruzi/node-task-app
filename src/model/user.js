a9db7ce2502d9577f6f44a4cc79c1d7007d82449
Totiruzi <totiruzi@yahoo.com>
2020-10-29 16:28:16 +0300

    Add Json web token to user object model

--- src/model/user.js
+++ src/model/user.js
@@ -1,8 +1,9 @@
-const validator = require('validator');
 const mongoose = require('mongoose');
+const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
@@ -20,6 +21,7 @@ const userSchema = mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     require: true,
     trim: true,
     lowercase: true,
@@ -40,31 +42,46 @@ const userSchema = mongoose.Schema({
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, 'nodefundamentals');
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+// Hash plain password before saving
 userSchema.pre('save', async function (next) {
   const user = this;
   if (user.isModified('password')) {
-    user.password = await bcrypt.hash('password', 8);
+    user.password = await bcrypt.hash(user.password, 8);
   }
   next();
 });
 
 const User = mongoose.model('User', userSchema);
-
-// const legit = new User({
-//   name: 'Bano',
-//   age: '35',
-//   email: 'frakego@lmail.com',
-//   password: 'Balanko',
-// });
-
-// legit
-//   .save()
-//   .then((ligit) => {
-//     console.log(legit, 'Successfuly saved!!!');
-//   })
-//   .then((error) => {
-//     console.log('Error', error);
-//   });
 module.exports = User;
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { update } = require('../model/user');
 const User = require('../model/user');
 const router = new express.Router();
 
@@ -7,9 +6,21 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(users);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send('Something went wrong!');
   }
 });
 
@@ -49,7 +60,7 @@ router.patch('/users/:id', async (req, res) => {
   const _id = req.params.id;
   const dataUpdate = req.body;
   try {
-    const user = await User.findByIdAndUpdate(_id);
+    const user = await User.findById(_id);
     updates.forEach((update) => (user[update] = dataUpdate[update]));
     await user.save();
 
